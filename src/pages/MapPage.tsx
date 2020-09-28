b3a79eec022aa843a35294e926abd95e31217685
Silich Iliya <iliya.s@goparrot.ai>
2020-09-28 13:33:38 +0300

    remove console.log

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -13,11 +13,8 @@ import { YMaps, Map, Placemark } from "react-yandex-maps";
       if( e.key === 'Enter' ){
          if(map) {
            const geoCode: any = map.geocode(query)
-           console.log("geoCode", geoCode)
            geoCode.then(
              function (res: any) {
-               console.log("res", res)
-
                const firstGeoObject = res.geoObjects.get(0)
                const coords = firstGeoObject.geometry.getCoordinates()
 
