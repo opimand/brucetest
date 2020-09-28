dacb9ff103ab7353b7b048cde4b01e668cb4b713
Silich Iliya <iliya.s@goparrot.ai>
2020-09-28 15:02:29 +0300

    Updates

---
+++ precache-manifest.72e6350a337fc9c2e2779612fdf7f989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f59266114b9708b0e86a0b7e1b5eab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a10a94532309aa69e0e",
+    "url": "/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "441605e3c92c94de09fc",
+    "url": "/static/css/main.429fddb0.chunk.css"
+  },
+  {
+    "revision": "3a10a94532309aa69e0e",
+    "url": "/static/js/2.ba7fb609.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ba7fb609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441605e3c92c94de09fc",
+    "url": "/static/js/main.c2b92937.chunk.js"
+  },
+  {
+    "revision": "ffcecf33cba4ed029c49",
+    "url": "/static/js/runtime-main.0ae1a967.js"
+  }
+]);
